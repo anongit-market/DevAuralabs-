0e471b76070ac72957aa43f9457eef608d77c841
Mayank <mayanksharma4174@gmail.com>
2025-10-19 14:56:14 +0000

    Try fixing this error: `Runtime Error: TypeError: Super expression must

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -1,3 +1,5 @@
+'use client';
+
 import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card';
 import { DollarSign, Users, BookOpen, BarChart } from 'lucide-react';
 import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from '@/components/ui/table';
