1697483f9d90d8dcc8f78ee7d515fd4965186d79
Mayank <mayanksharma4174@gmail.com>
2025-10-30 16:55:54 +0000

    Abe yrr Ask Aura ko default me gref karo na or footer ko hatao

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,6 @@
 
 import './globals.css';
 import { Toaster } from '@/components/ui/toaster';
-import Footer from '@/components/layout/footer';
 import Navbar from '@/components/layout/navbar';
 
 export default function RootLayout({
@@ -23,7 +22,6 @@ export default function RootLayout({
         <div className="relative z-10 flex min-h-screen flex-col">
           <Navbar />
           <main className="flex-1">{children}</main>
-          <Footer />
         </div>
         <Toaster />
       </body>
