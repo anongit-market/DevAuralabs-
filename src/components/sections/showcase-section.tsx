f6977b9c4ce7ced120216128e889dc866d77600b
Mayank <mayanksharma4174@gmail.com>
2025-10-31 07:26:19 +0000

    Maruqee efect khudse bnalo mannualy ya fir bina uske hi khuch karo

--- src/components/sections/showcase-section.tsx
+++ src/components/sections/showcase-section.tsx
@@ -2,7 +2,6 @@
 'use client';
 
 import Image from 'next/image';
-import { Marquee } from 'magic-ui-react';
 import { showcaseImages } from '@/lib/showcase-data';
 import { cn } from '@/lib/utils';
 
@@ -16,7 +15,7 @@ const ShowcaseCard = ({
   return (
     <figure
       className={cn(
-        'relative w-48 h-48 cursor-pointer overflow-hidden rounded-xl border p-4',
+        'relative w-48 h-48 cursor-pointer overflow-hidden rounded-xl border p-4 mx-4',
         'border-gray-950/[.1] bg-gray-950/[.01] hover:bg-gray-950/[.05]',
         'dark:border-gray-50/[.1] dark:bg-gray-50/[.10] dark:hover:bg-gray-50/[.15]'
       )}
@@ -35,16 +34,16 @@ const ShowcaseCard = ({
 };
 
 export default function ShowcaseSection() {
-    const firstRow = showcaseImages.slice(0, showcaseImages.length);
+    const duplicatedImages = [...showcaseImages, ...showcaseImages];
   
     return (
       <section id="showcase" className="py-12 md:py-16">
-        <div className="relative flex h-full w-full flex-col items-center justify-center overflow-hidden rounded-lg bg-transparent">
-          <Marquee pauseOnHover className="[--duration:20s]">
-            {firstRow.map((image) => (
-              <ShowcaseCard key={image.id} img={image.url} alt={image.alt} />
+        <div className="relative w-full overflow-hidden bg-transparent">
+          <div className="flex w-max marquee-content">
+            {duplicatedImages.map((image, index) => (
+              <ShowcaseCard key={index} img={image.url} alt={image.alt} />
             ))}
-          </Marquee>
+          </div>
           <div className="pointer-events-none absolute inset-y-0 left-0 w-1/3 bg-gradient-to-r from-background"></div>
           <div className="pointer-events-none absolute inset-y-0 right-0 w-1/3 bg-gradient-to-l from-background"></div>
         </div>
