6038cf130ee6f8c4d63273c6cd3a15e2fec3ed02
Mayank <mayanksharma4174@gmail.com>
2025-11-04 15:07:03 +0000

    Nahi ho parha hai login kya database se nahi connect ho rha hai

--- src/firebase/server.ts
+++ src/firebase/server.ts
@@ -4,22 +4,19 @@ import { initializeApp, getApps, getApp, FirebaseApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 
+// This function is safe to call on the server
+export function initializeFirebase() {
+  if (getApps().length) {
+    return getSdks(getApp());
+  }
+  const firebaseApp = initializeApp(firebaseConfig);
+  return getSdks(firebaseApp);
+}
+
 function getSdks(firebaseApp: FirebaseApp) {
   return {
     firebaseApp,
     auth: getAuth(firebaseApp),
     firestore: getFirestore(firebaseApp)
   };
-}
-
-// This function is safe to call on the server
-export function initializeFirebase() {
-  if (!getApps().length) {
-    // We are on the server, so we can use the config directly
-    const firebaseApp = initializeApp(firebaseConfig);
-    return getSdks(firebaseApp);
-  }
-
-  // If already initialized, return the SDKs with the already initialized App
-  return getSdks(getApp());
 }
